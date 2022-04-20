15a7a9c91e70372b302e75651c94c70d91a6e49e
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-04-20 11:51:46 +0700

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,64 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <nav className="flex justify-between text-center py-5 p-3 bg-blue-800 text-white shadow-sm antialiased	">
-        <h1 className="font-bold 4xl uppercase">
-          Division<span className="text-blue-300">siriraj</span>
-        </h1>
-        <ul className="flex space-x-4">
-          <li>
-            <Link href="/posts/first-post">
-              <a>ข่าวสาร</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/posts/first-post">
-              <a>การบริการ</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/posts/first-post">
-              <a>เกี่ยวกับเรา</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/posts/first-post">
-              <a>ติดต่อเรา</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav class="container flex justify-around py-8 mx-auto bg-gradient-to-r from-blue-900 to-blue-600 text-white">
+      <div class="flex items-center">
+        <img
+          src="https://www.si.mahidol.ac.th/th/division/assets_template/img/Siriraj_Logo.png"
+          alt=""
+        />
+      </div>
+
+      <div class="items-center hidden space-x-8 lg:flex">
+        <a href="">หน้าหลัก</a>
+        <a href="">เกี่ยวเรา</a>
+        <a href="">ข่าวสาร</a>
+        <a href="">ติดต่อเรา</a>
+        <a href="">ดาวน์โหลด</a>
+      </div>
+
+      <div class="flex items-center space-x-2">
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-6 h-6 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
+        </a>
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-6 h-6 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </a>
+      </div>
+    </nav>
   );
 }
