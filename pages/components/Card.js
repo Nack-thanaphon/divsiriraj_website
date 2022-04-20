2444d242ecc9ac8175bfaa83007c6c51aa10936b
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-04-20 14:59:01 +0700

    testtailwindcss

---
+++ pages/components/Card.js
@@ -0,0 +1,10 @@
+
+import Link from 'next/link'
+
+const Card = () => (
+  <div className='card'>
+    <h1>lorem</h1>
+  </div>
+)
+
+export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,44 @@
-import Head from "next/head";
-import Link from "next/link";
+// import Head from "next/head";
+// import Link from "next/link";
+
+import Card from "./components/Card";
 
 export default function Home() {
   return (
-    <nav className="container flex justify-around py-8 mx-auto bg-gradient-to-r from-blue-900 to-blue-600 text-white">
-      <div className="flex items-center">
-        <img
-          src="https://www.si.mahidol.ac.th/th/division/assets_template/img/Siriraj_Logo.png"
-          alt=""
-        />
-      </div>
+    <div className="contianer">
+      <nav className=" flex justify-around py-8 mx-auto bg-gradient-to-r from-teal-900 to-blue-600 text-white">
+        <div className="flex items-center">
+          <img
+            src="https://www.si.mahidol.ac.th/th/division/assets_template/img/Siriraj_Logo.png"
+            alt=""
+          />
+        </div>
 
-      <div className="items-center hidden space-x-8 lg:flex">
-        <a href="">หน้าหลัก</a>
-        <a href="">เกี่ยวเรา</a>
-        <a href="">ข่าวสาร</a>
-        <a href="">ติดต่อเรา</a>
-        <a href="">ดาวน์โหลด</a>
-      </div>
+        <div className="items-center hidden space-x-8 lg:flex">
+          <a href="">หน้าหลัก</a>
+          <a href="">เกี่ยวเรา</a>
+          <a href="">ข่าวสาร</a>
+          <a href="">ติดต่อเรา</a>
+          <a href="">ดาวน์โหลด</a>
+        </div>
 
-      <div className="flex items-center space-x-2"></div>
-    </nav>
+        <div className="flex items-center space-x-2"></div>
+      </nav>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-3 lg:grid-cols-6 bg-gray mx-auto p-4">
+        <div className="bg-gradient-to-r from-green-500 to-blue-400">1</div>
+        <div className="bg-gradient-to-r from-green-500 to-blue-400">1</div>
+        <div className="bg-gradient-to-r from-green-500 to-blue-400">1</div>
+        <div className="bg-gradient-to-r from-green-500 to-blue-400">1</div>
+        <div className="bg-gradient-to-r from-green-500 to-blue-400">1</div>
+        <div className="bg-gradient-to-r from-green-500 to-blue-400">1</div>
+        <div className="bg-gradient-to-r from-green-500 to-blue-400">1</div>
+
+        <Card />
+      </div>
+      <footer className="text-center my-4 bg-blue-500 p-6 fix-bottom">
+        <p className="text-white">คณะแพทยศาสตร์ศิริราช มหาวิทยาลัยมหิดล</p>
+      </footer>
+    </div>
   );
 }
