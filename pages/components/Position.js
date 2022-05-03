1007c9f7c55763e91aa28137696caeed193c220a
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-05-03 16:15:55 +0700

    update ui

--- pages/components/Position.js
+++
@@ -1,9 +0,0 @@
-class Position {
-    constructor(Position, Date, Firstname, Lastname) {
-        this.Position = Position;
-        this.date = Date;
-        this.Firstname = Firstname;
-        this.Lastname = Lastname;
-    }
-
-}
