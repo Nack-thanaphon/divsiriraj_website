dc913fc88421b74ceaf0cf9d6d7e84b30657c525
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-07-07 16:29:22 +0700

    ประกาศรับสมัครงาน

--- pages/index.js
+++ pages/index.js
@@ -20,28 +20,95 @@ export default function Home() {
                 <p>ฝ่ายวิศวกรรมบริการและอาคารสถานที่</p>
                 <small>ข้อมูลประจำปี 2565</small>
               </div>
-              <Link href="./employee" className="col-sm-8 col-12">
+              <div className="col-sm-8 col-12">
+                <div className="row m-0 p-0 ">
+                  <div className=" col-sm-4">
+                    <h3>อุบัติเหตุ</h3>
+                    <h1>0</h1>
+                  </div>
+                  <div className=" col-sm-4">
+                    <h3>ข้อร้องเรียน</h3>
+                    <h1>2</h1>
+                  </div>
+                  <div className=" col-sm-4">
+                    <h3>งานแก้ปัญหา</h3>
+                    <h1>1,000</h1>
+                  </div>
+                </div>
+              </div>
+              {/* <Link href="./employee" className="col-sm-8 col-12">
                 <div className="row m-0 p-0">
                   <div className="col-sm-4 col-12">
                     <p className="text-primary">จำนวนพนักงาน</p>
-                    <h1 className="fw-bold">20</h1>
+                    <h3 className="fw-bold">20</h3>
+
                   </div>
                   <div className="col-sm-4 col-12">
                     <p className="text-primary">จำนวนหน่วยงาน</p>
-                    <h1 className="fw-bold">8</h1>
+                    <h3 className="fw-bold">8</h3>
                   </div>
                   <div className="col-sm-4 col-12">
                     <p className="text-primary">ปัญหาที่พบเจอ</p>
-                    <h1 className="fw-bold">0</h1>
+                    <h3 className="fw-bold">0</h3>
                   </div>
                 </div>
-              </Link>
+              </Link> */}
             </div>
           </div>
 
           <div className="col-12 col-sm-4 m-0 p-1">
             <div className="card p-2 bg-light shadow-sm">
-              <h1 className="text-primary">ข่าวสาร</h1>
+              <h3 className="text-primary">เกี่ยวกับเรา</h3>
+              <p className="text-uppercase fw-bold">about us</p>
+
+              <Link href="./[id].js">
+                <a>อ่านเพิ่มเติม</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-sm-4 m-0 p-1">
+            <div className="card p-2 bg-light shadow-sm">
+              <h3 className="text-primary">บุคคลากร</h3>
+              <p className="text-uppercase fw-bold">employee</p>
+
+              <Link href="./[id].js">
+                <a>อ่านเพิ่มเติม</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-sm-4 m-0 p-1">
+            <div className="card p-2 bg-light shadow-sm">
+              <h3 className="text-primary">แจ้ง-ร้องเรียน</h3>
+              <p className="text-uppercase fw-bold">report-complaint</p>
+
+              <Link href="./[id].js">
+                <a>อ่านเพิ่มเติม</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-sm-4 m-0 p-1">
+            <div className="card p-2 bg-light shadow-sm">
+              <h3 className="text-primary">ติดต่อ</h3>
+              <p className="text-uppercase fw-bold">contract us</p>
+
+              <Link href="./[id].js">
+                <a>อ่านเพิ่มเติม</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-sm-4 m-0 p-1">
+            <div className="card p-2 bg-light shadow-sm">
+              <h3 className="text-primary">หน่วยงานภายใน</h3>
+              <p className="text-uppercase fw-bold">internal department</p>
+
+              <Link href="./[id].js">
+                <a>อ่านเพิ่มเติม</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-sm-4 m-0 p-1">
+            <div className="card p-2 bg-light shadow-sm">
+              <h3 className="text-primary">ข่าวสาร</h3>
               <p className="text-uppercase fw-bold">newsupdate</p>
 
               <Link href="./[id].js">
@@ -52,7 +119,7 @@ export default function Home() {
 
           <div className="col-12 col-sm-4 m-0 p-1">
             <div className="card p-2 bg-light shadow-sm">
-              <h1 className="text-primary">ข้อมูลสถิติ</h1>
+              <h3 className="text-primary">ข้อมูลสถิติ</h3>
               <p className="text-uppercase fw-bold">Data-stactic</p>
 
               <Link href="./static" className="btn btn-success">
@@ -62,7 +129,7 @@ export default function Home() {
           </div>
           <div className="col-12 col-sm-4 m-0 p-1">
             <div className="card p-2 bg-light shadow-sm">
-              <h1 className="text-primary">วาระการประชุม</h1>
+              <h3 className="text-primary">วาระการประชุม</h3>
               <p className="text-uppercase fw-bold">Agenda</p>
               <Link href="./[id].js" className="btn btn-success">
                 <a>อ่านเพิ่มเติม</a>
@@ -72,18 +139,107 @@ export default function Home() {
           <div className="row m-0 p-0">
             <div className="col-12 col-md-8  p-1">
               <div className="card h-100 p-3">
-                <h3 className="p-0 my-2">ประมวณภาพกิจกรรม</h3>
+                <h3 className="p-0 my-2">ประกาศรับสมัครงาน</h3>
+                <hr />
                 <ul className="m-0 p-0">
-                  <Activity />
-                  <Activity />
-                  <Activity />
-                  <Activity />
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
+                  <li className="list-unstyled mb-3 ">
+                    <p className="p-0 m-0 fw-bold">
+                      ตำแหน่ง : นักวิชาการทั่วไป
+                    </p>
+                    <small>สถานะ : ประกาศรับสมัคร</small>
+                    <br />
+                    <small>ตั้งแต่วันที่ : 15-20 มิถุนายน 2565</small>
+                    <br />
+                    <a href="">ดาวน์โหลดเอกสาร</a>
+                  </li>
                 </ul>
               </div>
             </div>
             <div className="col-12 col-md-4 p-1">
               <div className="card p-3">
                 <h3 className="p-0 my-2">กิจกรรมที่กำลังมา</h3>
+                <hr />
+
                 <ul className="m-0 p-0">
                   <Activity />
                   <Activity />
@@ -92,7 +248,9 @@ export default function Home() {
                 </ul>
               </div>
               <div className="card p-3 mt-sm-1">
-                <h3 className="p-0 my-2">ประกาศรับสมัครงาน</h3>
+                <h3 className="p-0 my-2">ประมวณภาพกิจกรรม</h3>
+                <hr />
+
                 <ul className="m-0 p-0">
                   <Activity />
                   <Activity />
